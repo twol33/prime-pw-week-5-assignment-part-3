51125b09978efc65a8e35bc8fd8bdf513803606b
Tou Xiong <tlxiong.prime@outlook.com>
2021-08-15 14:22:14 -0500

    completed addToCollection function that produces wanted results

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,13 +2,19 @@ console.log('***** Music Collection *****')
 let collection = []
 
 function addToCollection(title, artist, yearPublished){
-  if( collection.length <= 0)
-  collection.push( title, artist, yearPublished)
   let musicBox = {
     title: title,
     artist: artist,
     yearPublished: yearPublished,
   }
+  collection.push( musicBox)
   return musicBox;
 }
-console.log('adding first album:', addToCollection('Budda', 'James Pollock', '2021'));
+console.log('Album 1: ', addToCollection('Budda', 'James Pollock', '2021'));
+console.log('Album 2: ', addToCollection('On', 'Arthur King', '2020'));
+console.log('Album 3: ', addToCollection('My', 'Bruce Waters', '2019'));
+console.log('Album 4: ', addToCollection('Sweet', 'Martha Soup', '2018'));
+console.log('Album 5: ', addToCollection('Yellow', 'Garth Creek', '2017'));
+console.log('Album 6: ', addToCollection('Corn', 'Joe Penninsula', '2016'));
+
+console.log(collection);
