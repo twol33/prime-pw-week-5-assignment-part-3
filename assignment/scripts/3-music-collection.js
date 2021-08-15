9399e955b58ab28c2565f58be80ce12de0bec887
Tou Xiong <tlxiong.prime@outlook.com>
2021-08-15 14:01:07 -0500

    created a possible addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,10 @@
 console.log('***** Music Collection *****')
+let collection = []
+
+let 
+function addToCollection(title, artist, yearPublished){
+  if( collection.length <= 0)
+  collection.push(title, artist, yearPublished)
+  return collection;
+}
+console.log('adding first album:', addToCollection('Budda', 'James Pollock', '2021'));
